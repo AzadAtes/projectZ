d42d532b650fa094ac458a3457232d1f930713c1
Az <az.dev@e.mail.de>
2024-01-23 11:13:05 +0100

    add basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <p>Hello World</p>
+	<div
+		id="header-wrapper"
+		class="fixed left-0 top-0 flex h-header w-full justify-center"
+	>
+		<div id="header" class="flex flex-1 p-3"></div>
+	</div>
+	<div
+		id="main-content-wrapper"
+		class="mt-header flex h-full w-full justify-center"
+	>
+		<div
+			id="main-content"
+			class="min-w-main-content max-w-main-content flex-1 p-3"
+		>
+			<h1>HELLO WORLD</h1>
+			<RouterView />
+		</div>
+	</div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,vue}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+	content: ["./index.html", "./src/**/*.{js,ts,vue}"],
+	theme: {
+		extend: {
+			minWidth: {
+				"main-content": "300px",
+			},
+			maxWidth: {
+				"main-content": "1200px",
+			},
+			height: {
+				header: "60px",
+			},
+			margin: {
+				header: "60px",
+			},
+		},
+	},
+	plugins: [],
 };
